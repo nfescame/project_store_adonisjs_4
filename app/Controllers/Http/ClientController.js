c77ce6079de819cae2b63fcc41ce6793767ee577
nilton <nfescame@gmail.comq:qclear>
2022-07-12 10:42:59 -0300

    created address and phone migration

--- app/Controllers/Http/ClientController.js
+++ app/Controllers/Http/ClientController.js
@@ -3,6 +3,8 @@
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 const Client = use("App/Models/Client");
+const Phone = use("App/Models/Phone");
+const Address = use("App/Models/Address");
 
 /**
  * Resourceful controller for interacting with clients
@@ -18,7 +20,10 @@ class ClientController {
    * @param {View} ctx.view
    */
   async index({}) {
-    const clients = await Client.query().with("user").fetch();
+    const clients = await Client.query()
+      .select("id", "user_id", "name", "last_name", "created_at")
+      .with("user", (builder) => builder.select("id", "username", "email"))
+      .fetch();
 
     return clients;
   }
@@ -31,18 +36,48 @@ class ClientController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store({ request, auth }) {
-    const data = request.only([
+  async store({ request, auth, response }) {
+    const {
+      name,
+      last_name,
+      cpf,
+      rg,
+      birth_date,
+      phone_number,
+      street,
+      number,
+    } = request.only([
       "name",
       "last_name",
       "cpf",
       "rg",
       "birth_date",
-      "phone",
+      "phone_number",
+      "street",
+      "number",
     ]);
-    const client = await Client.create({ user_id: auth.user.id, ...data });
 
-    return client;
+    const client = await Client.create({
+      user_id: auth.user.id,
+      name,
+      last_name,
+      cpf,
+      rg,
+      birth_date,
+    });
+
+    const address = await Address.create({
+      client_id: client.id,
+      street,
+      number,
+    });
+
+    const phone = await Phone.create({
+      client_id: client.id,
+      phone: phone_number,
+    });
+
+    return response.status("200").json({ data: { client, address, phone } });
   }
 
   /**
@@ -56,7 +91,12 @@ class ClientController {
    */
   async show({ params }) {
     const client = await Client.query()
-      .with("user")
+      .select("id", "user_id", "name", "last_name", "cpf", "rg", "birth_date")
+      .with("user", (builder) => builder.select("id", "username", "email"))
+      .with("address", (builder) =>
+        builder.select("id", "client_id", "street", "number")
+      )
+      .with("phone", (builder) => builder.select("id", "client_id", "phone"))
       .where("id", params.id)
       .first();
 
